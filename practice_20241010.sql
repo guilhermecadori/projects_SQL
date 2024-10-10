19275533fe51736a2660f163472e485bd729b42a
guilhermecadori <gccadori@gmail.com>
2024-10-10 11:15:01 -0300

    New exercise set

--- practice_20241010.sql
+++ practice_20241010.sql
@@ -9,3 +9,37 @@ SELECT
       s
     , UPPER(s) AS res
 FROM makeuppercase
+
+
+-- Ex 2
+/*
+    For this challenge you need to create a simple SELECT statement that 
+    will return all columns from the people table, and join to the toys 
+    table so that you can return the COUNT of the toys.
+    
+    people table schema
+        id
+        name
+
+    toys table schema
+        id
+        name
+        people_id
+
+    You should return all people fields as well as the toy count as "toy_count".
+
+        NOTE: Your solution should use pure SQL. Ruby is used within the test 
+              cases to do the actual testing.
+*/
+
+SELECT 
+      p.id
+    , p.name
+    , CASE
+        WHEN COUNT(t.id) IS NULL THEN 0
+        ELSE COUNT(t.id) 
+      END AS toy_count
+FROM people p
+LEFT JOIN toys t
+ON p.id = t.people_id
+GROUP BY p.id
