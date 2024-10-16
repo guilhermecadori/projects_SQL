2e195b6e2f6207b285ad4d3e6606c2dfd54910bb
guilhermecadori <gccadori@gmail.com>
2024-10-16 10:01:33 -0300

    New function

--- practice_20241016.sql
+++ practice_20241016.sql
@@ -1,3 +1,5 @@
+-- Ex 1
+
 /*
     For this challenge you need to create a simple SELECT statement that 
     will return all columns from the products table, and join to the companies 
@@ -26,4 +28,30 @@ SELECT
     , c.name AS company_name
 FROM products p
 LEFT JOIN companies c
-ON p.company_id = c.id 
+ON p.company_id = c.id;
+
+
+-- Ex 2
+/*
+    For this challenge you need to create a basic Increment function 
+    which Increments on the age field of the peoples table.
+
+    The function should be called increment, it needs to take 1 integer 
+    and increment that number by 1.
+
+    You may query the people table while testing but the query must only 
+    contain the function on your final submit.
+    
+    Important: you must remove all comments when submitting the kata.
+    
+    people table schema
+        id
+        name
+        age
+*/
+CREATE FUNCTION increment(age INT)
+RETURNS INT
+LANGUAGE sql
+AS $$
+    SELECT age + 1;
+$$;
