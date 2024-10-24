8220e961617d6847655e009ec405cc4464f8db15
guilhermecadori <gccadori@gmail.com>
2024-10-24 10:39:02 -0300

    New functions (to me) being used

--- practice_20241024.sql
+++ practice_20241024.sql
@@ -1,8 +1,8 @@
 -- Ex 1
 SELECT
-    UNNEST(genres) as genre
+    UNNEST(genres) as genre -- Cool new function
   , COUNT(title) AS count
-  , ARRAY_AGG(title ORDER BY title ASC) as books
+  , ARRAY_AGG(title ORDER BY title ASC) as books -- Cool new function
 FROM books
 GROUP BY genre
 ORDER BY count DESC, genre ASC
