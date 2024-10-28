d2bf0985bc7d4e72f06bb853fc0b3342826a7afc
guilhermecadori <gccadori@gmail.com>
2024-10-28 08:46:00 -0300

    Updated old file and working on a new one

--- practice_20241024.sql
+++ practice_20241024.sql
@@ -1,8 +1,8 @@
 -- Ex 1
 SELECT
-    UNNEST(genres) as genre -- Cool new function
+    UNNEST(genres) AS genre -- Cool new function
   , COUNT(title) AS count
-  , ARRAY_AGG(title ORDER BY title ASC) as books -- Cool new function
+  , ARRAY_AGG(title ORDER BY title ASC) AS books -- Cool new function
 FROM books
 GROUP BY genre
 ORDER BY count DESC, genre ASC
---
+++ practice_20241028.sql
@@ -0,0 +1,38 @@
+-- Ex 1
+/*  
+    You have been provided with a PostgreSQL database that contains an employees table. 
+    
+    The employees table has the following structure:
+        id: an integer column that uniquely identifies each employee.
+        name: a text column that contains the name of the employee.
+        manager_id: an integer column that identifies the manager of each employee. 
+        The manager_id corresponds to an id in the same employees table. 
+        This means that our table has a recursive relationship with itself - 
+        an employee can be a manager to other employees. 
+        It is nullable: top managers do not have managers above them.
+
+    Your task is to write a SQL query that meets the following requirements:
+        The query should output a list of all managers and their respective employees, grouped by the manager_id.
+        The output of your query should be a table with two columns:
+            manager_id: ID of each manager.
+            employee_names: an array of all the names of employees who have that manager. 
+            Each entry in the array should be a string of the form employee_name (employee_id). 
+            The employees in each array should be ordered by their id in ascending order.
+        We should avoid displaying null for manager_id colummn in the result set.
+        The output rows should be ordered by manager_id in ascending order.
+
+    The desired output should look like this:
+
+    manager_id  | employee_names                                                       |
+    ------------+----------------------------------------------------------------------|
+     1          | {"Lewis Fadel (4)","Nicole Konopelski (7)","Dwayne Wilderman (10)"}  | 
+     2          | {"Katharina Ferry (43)","Williemae Ledner (79)"}                     |
+        ...
+*/
+
+SELECT
+      manager_id
+    , (ARRAY_AGG(name) ORDER BY employee_id ASC) AS employee_name
+FROM employee
+GROUP BY manager_id
+ORDER BY manager_id ASC
