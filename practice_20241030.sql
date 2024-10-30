acd7fafb299b743f705cc2a6bc37945b9fd89fce
guilhermecadori <gccadori@gmail.com>
2024-10-30 12:59:41 -0300

    Passed all tests

---
+++ practice_20241030.sql
@@ -0,0 +1,28 @@
+-- Ex 1
+/*
+    Given a table of random numbers as follows:
+    ** numbers table schema **
+        id
+        number1
+        number2
+        number3
+        number4
+        number5
+
+    Your job is to return a table in the following format, where each value 
+    is the number of bytes in the string representation of the number.
+    ** output table schema **
+        octnum1
+        octnum2
+        octnum3
+        octnum4
+        octnum5
+*/
+
+SELECT
+      OCTET_LENGTH(CAST(number1 AS TEXT)) AS octnum1
+    , OCTET_LENGTH(CAST(number2 AS TEXT)) AS octnum2
+    , OCTET_LENGTH(CAST(number3 AS TEXT)) AS octnum3
+    , OCTET_LENGTH(CAST(number4 AS TEXT)) AS octnum4
+    , OCTET_LENGTH(CAST(number5 AS TEXT)) AS octnum5
+FROM numbers
