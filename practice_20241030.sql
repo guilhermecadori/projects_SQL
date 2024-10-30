74c8eb5f470b059c88acb1968a00a81835c35319
guilhermecadori <gccadori@gmail.com>
2024-10-30 13:13:45 -0300

    New exercise; passed all tests

--- practice_20241030.sql
+++ practice_20241030.sql
@@ -26,3 +26,37 @@ SELECT
     , OCTET_LENGTH(CAST(number4 AS TEXT)) AS octnum4
     , OCTET_LENGTH(CAST(number5 AS TEXT)) AS octnum5
 FROM numbers
+
+
+-- Ex 2
+/*
+    You are the owner of the Grocery Store. All your products are in the database, 
+    that you have created after CodeWars SQL excercises!
+    You have reached a conclusion that you waste too much time because you have too 
+    many different warehouse to visit each week.
+    You have to find out how many different type of products you buy from each producer. 
+    If you take only a few items from some of them you will stop going there to save gasoline.
+    
+    In the results show producer and count_products_types which you buy from him.
+
+    Order the result by count_products_types (DESC) then by producer (ASC) in case there are duplicate amounts,
+
+    products table schema
+        id
+        name
+        price
+        stock
+        producer
+
+    results table schema
+        count_products_types
+        producer
+*/
+SELECT
+      producer
+    , COUNT(id) AS count_products_types
+FROM products
+GROUP BY producer
+ORDER BY 
+      count_products_types DESC
+    , producer ASC
